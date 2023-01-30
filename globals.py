54496f75f234ddec53308971bd0c1be4c88b7914
Pawan Jaiswal <pawan.jaiswal09@gmail.com>
2023-01-30 16:25:46 -0600

    Adding globals expense map for categorizing expenses

---
+++ globals.py
@@ -0,0 +1,9 @@
+expense_map = {"Groceries": ["MART", "TARGET", "JEWEL OSCO", "WALGREENS", "Market", "Patel"],
+               "Utilities": ["ATT", "CITY OF CRYSTAL LAKE", "Nicor Gas", "COMED",
+                             "RECYCLING", "RING"],
+               "Entertainment": ["Peacock", "AMC", "Netflix", "Hulu", "Bowlero"],
+               "Restaurants": ["Ramen", "Biryani", "Pizza", "Taco", "Chipotle", "Subway",
+                               "Annapurna", "Starbucks", "Eggholic"],
+               "Car": ["Thornton", "STATE FARM", "Avis"],
+               "Travel": ["Frontier", "Etihad", "American", "United", "Ventra", "Metra", "Uber",
+                          "Spothero"]}
