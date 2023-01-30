388bc41f47dc70b4a55e3dfc7fcf0e256d9a99e5
Pawan Jaiswal <pawan.jaiswal09@gmail.com>
2023-01-30 16:04:23 -0600

    Add logic to read PDF statement and filter transactions

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
+from PyPDF2.errors import PdfReadError
 import PyPDF2
 
+txn_start = '$ Amount'
+txn_end = 'Total fees charged'
+
 if __name__ == '__main__':
-    fileName = 'stmt/20221224-statements-2577-.pdf'
-    pdfFileObj = open(fileName, 'rb')
-    pdfReader = PyPDF2.PdfReader(pdfFileObj)
-    print(f'Total Pages in {fileName.split("/")[-1]}: {len(pdfReader.pages)}')
+    file_name = 'stmt/20221224-statements-2577-.pdf'
+    file_obj = open(file_name, 'rb')
+    pdfReader = PyPDF2.PdfReader(file_obj, strict=False)
+    print(f'Total Pages in {file_name.split("/")[-1]}: {len(pdfReader.pages)}')
+    txn_found = False
+    split_transactions = []
+    for page_num in range(len(pdfReader.pages)):
+        try:
+            page_content = pdfReader.pages[page_num]
+            transactions = page_content.extract_text()
+            if (transactions.find(txn_start) > 0 and not txn_found) or txn_found:
+                txn_found = True
+                raw_transactions = transactions[transactions.find(txn_start)+len(txn_start) :
+                                                transactions.find(txn_end)]
+                split_transactions.extend(raw_transactions.splitlines())
+                [print(x) for x in split_transactions]
+        except PdfReadError:
+            txn_found = False
