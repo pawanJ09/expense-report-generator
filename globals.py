6b88a465f731867fac1dc965c479dd103e91d60e
Pawan Jaiswal <pawan.jaiswal09@gmail.com>
2023-01-30 16:29:36 -0600

    Adding Home category in globals expense map

--- globals.py
+++ globals.py
@@ -6,4 +6,5 @@ expense_map = {"Groceries": ["MART", "TARGET", "JEWEL OSCO", "WALGREENS", "Marke
                                "Annapurna", "Starbucks", "Eggholic"],
                "Car": ["Thornton", "STATE FARM", "Avis"],
                "Travel": ["Frontier", "Etihad", "American", "United", "Ventra", "Metra", "Uber",
-                          "Spothero"]}
+                          "Spothero"],
+               "Home": ["Home Depot", "Lowe", "Menards", "Woodlore Estates"]}
