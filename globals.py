9ce10e81846d51995e2283595d98fcf36f91f52c
Pawan Jaiswal <pawan.jaiswal09@gmail.com>
2023-02-06 09:51:16 -0600

    Updating groceris and car expense categories with additional vendors

--- globals.py
+++ globals.py
@@ -1,5 +1,5 @@
 expense_map = {"Groceries": ["MART", "TARGET", "JEWEL OSCO", "WALGREENS", "Market", "Patel",
-                             "Carniseria La Rosita", "COSTCO"],
+                             "Carniseria La Rosita", "COSTCO WHSE"],
                "Utilities": ["ATT", "CITY OF CRYSTAL LAKE", "Nicor Gas", "COMED",
                              "RECYCLING", "RING", "COMCAST"],
                "Entertainment": ["Peacock", "AMC", "Netflix", "Hulu", "Bowlero"],
@@ -7,7 +7,7 @@ expense_map = {"Groceries": ["MART", "TARGET", "JEWEL OSCO", "WALGREENS", "Marke
                         "Annapurna", "Starbucks", "Eggholic", "Portillos", "CULVERS",
                         "El Sombrero", "Nando", "KALEIDOSCOOPS", "Wraps", "OBERWEIS",
                         "RICHARD WALKERS", "Burrito"],
-               "Car": ["Thornton", "STATE FARM", "Avis", "SPEEDWAY"],
+               "Car": ["Thornton", "STATE FARM", "Avis", "SPEEDWAY", "COSTCO GAS"],
                "Travel": ["Frontier", "Etihad", "American", "United", "Ventra", "Metra", "Uber",
                           "Spothero", "Booking"],
                "Home": ["Home Depot", "Lowe", "Menards", "Woodlore Estates"]}
