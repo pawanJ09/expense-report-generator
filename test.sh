85dc09243aefb8ddb6e5f32e37525da4265855d0
Pawan Jaiswal <pawan.jaiswal09@gmail.com>
2023-02-10 10:02:50 -0600

    Updating script to check for Lambda failures

--- test.sh
+++ test.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+# AWS CLI v1 command
 error_message=$(aws lambda invoke --function-name expense-report-generator \
 --invocation-type RequestResponse --payload file://events/test-sqs-event.json \
---cli-binary-format raw-in-base64-out /tmp/lambda-response.txt | grep "errorMessage")
+/tmp/lambda-response.txt | grep "FunctionError")
   # Exit if error received from Lambda invocation
 echo "$error_message"
 
-if [ -n "$error_message" ]
+if [ -z "$error_message" ]
 then
   echo "Success returned from Lambda"
   exit 0
